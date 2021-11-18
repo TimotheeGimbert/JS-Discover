32aa4a1f32b7ce722cf998510981959ea9f2d3cf
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-18 12:38:40 +0100

    adds factorial function

---
+++ script_2.js
@@ -0,0 +1,17 @@
+function factorial(num) {
+    if (num == 0) { 
+        return 1; 
+    }; 
+    let result = num;
+    num -= 1;
+    while (num !== 0) {
+        result = result * (num);
+        num -= 1;
+    }
+    return result;
+} 
+
+let chosenNumber = prompt("De quel nombre veux-tu calculer la factorielle ?");
+
+let result = factorial(chosenNumber);
+console.log(`Le résultat est ${result} !`)
