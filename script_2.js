949516765ed5ae752281dcae1724322c10b0d5c8
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-18 12:40:34 +0100

    adds negative cases factorial

--- script_2.js
+++ script_2.js
@@ -2,6 +2,9 @@ function factorial(num) {
     if (num == 0) { 
         return 1; 
     }; 
+    if (num < 0) { 
+        return -1; 
+    };
     let result = num;
     num -= 1;
     while (num !== 0) {
